7f78813f9af22d9f8bca82c3567a5536cccc3fcd
Aldriculteur <aldric.claude@hotmail.fr>
2023-11-19 22:19:53 +0100

    Ajout des boutons et de l'affichage de la date et du changement de vue week/day

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
-import { CalendarEvent, CalendarNativeDateFormatter, CalendarView, DateFormatterParams } from 'angular-calendar';
+import { CalendarEvent, CalendarView } from 'angular-calendar';
 import localeFr from '@angular/common/locales/fr'
 import { Subject } from 'rxjs';
 
@@ -16,37 +16,50 @@ export class CalendarComponent {
   view: CalendarView = CalendarView.Week;
   viewDate: Date = new Date();
   calendarView = CalendarView;
-  excludeDays: number[] = [0, 6];
+  excludeDays: number[] = [0, 6]; // Exclue dimanche et samedi
   events: CalendarEvent[] =[];
   dayStartHour: number = 8; // Demarre à 8h
   dayEndHour: number = 18;  // Termine à 19h
-  hourSegments: number = 4; 
+  hourSegments: number = 4; // une ligne tout les quart d'heure
 
   refresh = new Subject<void>;
-  constructor() {
- 
 
-    const event1 ={
-      title: "BDD",
-      start: new Date("2023-11-22T10:30"),
-      end: new Date("2023-11-22T12:30"),
+  constructor() {
+    const event0 ={
+      title: "Anglais",
+      start: new Date("2023-11-23T12:30"),
+      end: new Date("2023-11-23T14:00"),
       draggable: true,
       resizable: {
         beforeStart: true,
         afterEnd: true
       }
-    }
-    this.events.push(event1);
+    } 
+    this.events.push( event0 );
   }
+
   setView(view: CalendarView) {
     this.view = view;
   }
-  eventClicked(event: any) {
-    console.log(event);
 
-  }
+  eventClicked() {
+    if (this.view === this.calendarView.Day ){
+      this.setView(this.calendarView.Week);
+    } else {
+      this.setView(this.calendarView.Day);
+      }
+    }
+    dayViewClick() {
+      this.setView(this.calendarView.Week);
+    }
+    changeDay(date: Date) {
+      this.viewDate = date;
+      this.view = CalendarView.Day;
+    }
+
   eventTimesChanged(event: any) {
-    event.event.start = event.newStart;
+    //TODO Renvoyez les nouvelles dates à la BD
+    event.event.start = event.newStart; 
     event.event.end = event.newEnd;
     this.refresh.next();
   }
